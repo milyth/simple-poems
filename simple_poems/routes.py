a29454cff0f1a0ab345317ba78e4090ff8d37da6
Moizes J. Sousa <milyth@duck.com>
2023-05-21 17:26:50 -0300

    feat: website hahaah

--- simple_poems/routes.py
+++ simple_poems/routes.py
@@ -1,10 +1,14 @@
-from fastapi import Response, status, APIRouter
+from fastapi import Response, status, APIRouter, Request, Form
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import RedirectResponse
 from aiosqlite import IntegrityError
+from typing import Annotated
 
 from .world import insertPoem, listPoems
 from .schemas import Poem
 router = APIRouter()
+templates = Jinja2Templates(directory="templates")
+
 
 @router.get("/api/v1/list")
 async def list():
@@ -28,4 +32,21 @@ async def push(poem: Poem, response: Response):
         response.status_code = status.HTTP_409_CONFLICT
         return {'status': "fail", 'code': 409, 'error': 'ALREADY_EXISTS'}
     
+
+@router.get("/_upload")
+async def upload(title: str, author: str, content: str):
+    try:
+        await insertPoem(Poem(title=title, author=author, content=content))
+    except IntegrityError:
+       pass
+    return RedirectResponse(url="/")
     
+    
+
+@router.get("/")
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", context={'request': request, 'poems]': await listPoems()})
+
+@router.get("/upload")
+async def upload(request: Request):
+    return templates.TemplateResponse("upload.html", context={'request': request})
